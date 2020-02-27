9a8ad907f164942d4b90c26ddb1256495ffc15ad
ShivaKumar3009 <60172352+ShivaKumar3009@users.noreply.github.com>
2020-02-27 23:28:33 +0530

    Update House_main.java

--- serialisation_Task4/src/main/java/House_construction/House_main.java
+++ serialisation_Task4/src/main/java/House_construction/House_main.java
@@ -4,24 +4,18 @@ import java.util.*;
 public class House_main {
 	public static void main(String [] args) {
 		Scanner s=new Scanner(System.in);
-		System.out.println("Enter material type");
-		System.out.println("\n1.Standard\n2.AboveStandard\n3.HighStandard\n4.HighStandardandAutomated");
-		String material=s.next();
-		System.out.println("Enter area of house");
-		int house_area=s.nextInt();
-		double cost;
-		House h=new House(material);
-		int type = 0;
-		if (!(material =="Standard"))
-			 type=1;
-		else if (!(material =="AboveStandard"))
-			 type=2;
-		else if (!(material =="HighStandard"))
-			 type=3;
-		else if (!(material =="HighStandardandAutomated"))
-			 type=4;
-				
-			switch(type) {
+		
+		
+		while (true){
+			System.out.println("Enter area of house");
+			int house_area=s.nextInt();
+			double cost;
+			System.out.println("Enter material type (INTEGER)");
+			System.out.println("\n1.Standard\n2.AboveStandard\n3.HighStandard\n4.HighStandardandAutomated\n5.exit\n");
+			int material=s.nextInt();
+			House h=new House(material);
+			
+			switch(material) {
 			
 			case 1: cost=1200*house_area;
 					System.out.println("cost of house construction :"+cost);
@@ -37,11 +31,12 @@ public class House_main {
 			case 4: cost=2500*house_area;
 					System.out.println("cost of house construction :"+cost);
 					break;
+			case 5: return;
 				
-			default: System.out.println("No such material");
-					 System.out.println("\n1.Standard\n2.AboveStandard\n3.HighStandard\n4.HighStandardandAutomated");
-						break ;
+			default: System.out.println("No such material\nEnter again\n");
+				 break ;
 			}
+		}
 		
 		
 	}
